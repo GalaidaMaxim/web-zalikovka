fc5f77108ccdc8005539449cb00379ccd180c52b
Maxim Kucheriaviy <maxim-k@i.ua>
2025-05-23 14:01:32 +0300

    migration fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import {
   Box,
   useMediaQuery,
   useTheme,
+  Drawer,
 } from "@mui/material";
 import styled from "@emotion/styled";
 import { NavLink } from "react-router-dom";
@@ -14,11 +15,12 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logoutOperation } from "../../redux/operations";
 import { MobileNvaigation } from "./MobileNavigation";
+import { useState } from "react";
 
 const StyledHeader = styled(AppBar)`
   height: 4rem;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
 
   justify-content: space-between;
@@ -38,12 +40,41 @@ export const Header = () => {
   const dispatch = useDispatch();
   const theme = useTheme();
   const isPhone = useMediaQuery(theme.breakpoints.down("tablet"));
+  const [isOpen, setIsOpen] = useState(true);
 
   const logout = () => {
     dispatch(logoutOperation(token));
   };
   return (
     <StyledHeader position="static">
+      <Drawer open={isOpen} onClick={() => setIsOpen(false)}>
+        <Box
+          sx={{ padding: "20px", backgroundColor: "black", cursor: "pointer" }}
+          position="fixed"
+        >
+          <Typography variant="body2">
+            <Typography sx={{ color: "yellow" }} component="span" variant="h2">
+              {"Увага   "}
+            </Typography>{" "}
+            Web заліковка мігрує. Актуальна версія доступна за посиланням
+            <Typography component="span" variant="body2">
+              {" "}
+              <a
+                style={{ color: "cyan" }}
+                href="https://web-zalikowka-next-93au.vercel.app/ "
+              >
+                https://web-zalikowka-next-93au.vercel.app/
+              </a>{" "}
+              Актуальні оновлення будуть доступні на новому домені, тому
+              рекомендуємо використовувати саме його. Поточна сторінка буде
+              доступна протягом 2025 року.
+            </Typography>
+          </Typography>
+          <Typography sx={{ marginTop: "20px" }} variant="body2">
+            Натисньть щоб закрити
+          </Typography>
+        </Box>
+      </Drawer>
       <ContainerCustom
         sx={{
           height: "4rem",
